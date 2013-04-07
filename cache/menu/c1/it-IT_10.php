466b6df1f5a2a57ef6b07ca02a3232f42d970684
Maurizio Carboni <maury91@gmail.com>
2013-04-07 22:11:28 +0200

    cached_created_by_ALExxia

---
+++ cache/menu/c1/it-IT_10.php
@@ -0,0 +1,110 @@
+<?php
+$menu = array (
+  'top' => 
+  array (
+    0 => 
+    array (
+      'type' => true,
+      'level' => 10,
+      'class' => '',
+      'image' => '',
+      'text' => '',
+      'lang' => 'all',
+      'links' => 
+      array (
+        0 => 
+        array (
+          'text' => 'Link1',
+          'href' => '#',
+          'lang' => 'all',
+          'class' => '',
+          'image' => '',
+          'level' => 10,
+        ),
+        1 => 
+        array (
+          'text' => 'Link2',
+          'href' => '#',
+          'lang' => 'all',
+          'class' => '',
+          'image' => '',
+          'level' => 10,
+        ),
+        2 => 
+        array (
+          'text' => 'Link3',
+          'href' => '#',
+          'lang' => 'it-IT',
+          'class' => '',
+          'image' => '',
+          'level' => 10,
+        ),
+        3 => 
+        array (
+          'text' => 'Link4',
+          'href' => '#',
+          'lang' => 'it-IT',
+          'class' => '',
+          'image' => '',
+          'level' => 10,
+        ),
+      ),
+    ),
+  ),
+  'right' => 
+  array (
+    0 => 
+    array (
+      'type' => true,
+      'level' => 10,
+      'class' => '',
+      'image' => '',
+      'text' => '',
+      'lang' => 'all',
+      'links' => 
+      array (
+        0 => 
+        array (
+          'text' => 'Link1',
+          'href' => '#',
+          'lang' => 'all',
+          'class' => '',
+          'image' => '',
+          'level' => 10,
+        ),
+        1 => 
+        array (
+          'text' => 'Link2',
+          'href' => '#',
+          'lang' => 'all',
+          'class' => '',
+          'image' => '',
+          'level' => 10,
+        ),
+        2 => 
+        array (
+          'text' => 'Link4',
+          'href' => '#',
+          'lang' => 'all',
+          'class' => '',
+          'image' => '',
+          'level' => 10,
+        ),
+      ),
+    ),
+  ),
+  'slider' => 
+  array (
+    0 => 
+    array (
+      'type' => false,
+      'level' => 10,
+      'class' => '',
+      'image' => '',
+      'text' => '',
+      'lang' => 'all',
+      'module' => 'mauro_banner',
+    ),
+  ),
+);
+?>
---
+++ cache/menu/c2/it-IT_8.php
@@ -0,0 +1,7 @@
+<?php
+$menu = array (
+  'top' => '<li ><a href=\'#\'>Link1</a></li><li ><a href=\'#\'>Link2</a></li><li ><a href=\'#\'>Link3</a></li><li ><a href=\'#\'>Link4</a></li>',
+  'right' => '<div ></div><nav><ul><li ><a href=\'#\'>Link1</a></li><li ><a href=\'#\'>Link2</a></li><li ><a href=\'#\'>Link3</a></li><li ><a href=\'#\'>Link4</a></li></ul></nav>',
+  'slider' => '',
+);
+?>
