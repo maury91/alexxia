174843aa74c62cadb7a2606645419e2b1ecf09ee
Maurizio Carboni <maury91@gmail.com>
2013-04-07 22:43:37 +0200

    created_by_ALExxia

--- cache/menu/c2/it-IT_0.php
+++ cache/menu/c2/it-IT_0.php
@@ -2,6 +2,6 @@
 $menu = array (
   'top' => '<li ><a href=\'#\'>Link1</a></li><li ><a href=\'#\'>Link2</a></li><li ><a href=\'#\'>Link3</a></li><li ><a href=\'#\'>Link4</a></li>',
   'right' => '<div ></div><nav><ul><li ><a href=\'#\'>Link1</a></li><li ><a href=\'#\'>Link2</a></li><li ><a href=\'#\'>Link3</a></li><li ><a href=\'#\'>Link4</a></li></ul></nav>',
-  'slider' => '',
+  'slider' => ''.self::mod('mauro_banner').'',
 );
 ?>
