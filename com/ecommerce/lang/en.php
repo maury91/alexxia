b6c998cd276fbf7fefbafa2cf34f33797e7d1686
Matteo <matteozanda@hotmail.com>
2013-05-24 13:13:36 +0200

    lol

--- com/ecommerce/lang/en.php
+++ com/ecommerce/lang/en.php
@@ -1,7 +1,8 @@
 <?php
 $__prod_id = 'Product ID';
 $__prod_q = 'Quantity;';
-$__prod_p = 'Price';$__prod_tp = 'Total price';
+$__prod_p = 'Price';
+$__prod_tp = 'Total price';
 $__prod_pi = 'pieces';
 $__prod_w = 'Weight';
 $__prod_bn = 'Buy now';
@@ -10,4 +11,11 @@ $__prod_det = 'Product details';
 $__prod_shp = 'Shipments';
 $__prod_pay = 'Payments';
 $__prod_nf = 'Product not found!';
+$__cart_emp = 'Your cart is empty';
+$__cart_empt= 'Your cart is empty. Click "Add to Cart" on the interesting articles.';
+$__cart_rem = ' has been removed';
+$__remove 	= 'Remove';
+$__cart 	= 'Cart';
+$__proced	= 'Continue with the transation';
+$__cart_removed = 'The article has been removed'; 
 ?>
