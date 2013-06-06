4518ab9a89856ddc8e0b7f5022f2901226ebb5f0
Maurizio Carboni <maury91@gmail.com>
2013-06-06 11:16:05 +0200

    news

--- template/mauro/script.js
+++ template/mauro/script.js
@@ -1,4 +1,4 @@
-function log10 (arg) {
+/*function log10 (arg) {
   // http://kevin.vanzonneveld.net
   // +   original by: Philip Peterson
   // +   improved by: Onno Marsman
@@ -9,14 +9,14 @@ function log10 (arg) {
   // *     example 2: log10(1);
   // *     returns 2: 0
   return Math.log(arg) / 2.302585092994046; // Math.LN10
-}
+}*/
 
 $(function(){
 	$('body').mousemove(function(e) {
 		var off=$('.title_text').offset();
 		var xx = e.clientX-(off.left+234);
 		var yy = e.clientY-(off.top+52);
-		if (xx>1)
+		/*if (xx>1)
 			xx=-log10(xx);
 		else if (xx<-1)
 			xx=log10(-xx);
@@ -27,8 +27,10 @@ $(function(){
 		else if (yy<-1)
 			yy=log10(-yy);
 		else
-			yy=0;
+			yy=0;*/
+		xx/=-50;
+		yy/=-50;
 		//$('.title_text').css('text-shadow',xx*2+'px '+yy*2+'px 2px #777');
-		$('.title_text .shadow').css({left:xx*2,top:yy*2});
+		$('.title_text .shadow').css({left:xx,top:yy});
 	});
 });
