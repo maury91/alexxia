72ccd70aac58e6ad315c6a36caa09b78927a8e41
Maurizio Carboni <maury91@gmail.com>
2013-06-06 16:09:23 +0200

    news

--- com/ecommerce/payments/paypal/config/config.php
+++ com/ecommerce/payments/paypal/config/config.php
@@ -1,7 +1,11 @@
 <?php
 $paypal_email	= 'maury91@gmail.com';
-$ipn_code		= 'VoNt8H7VQFgnXV5q'
+$ipn_code		= 'VoNt8H7VQFgnXV5q';
+/*
 $paypal_api_user= 'maury91_api1.gmail.com';
 $paypal_api_pass= 'HSMVPDQUVH58HRCK';
-$paypal_api_sign= 'AWZOdFbkx.b1-IQT7vMaPfkdzkAeAeY8Q.Y5QglOP1acjLxXGlhGu6VC';
+$paypal_api_sign= 'AWZOdFbkx.b1-IQT7vMaPfkdzkAeAeY8Q.Y5QglOP1acjLxXGlhGu6VC';*/
+$paypal_api_user= 'maury91-facilitator_api1.gmail.com';
+$paypal_api_pass= '1369844971';
+$paypal_api_sign= 'APMcQYQ4B6GG5djyh-OFvd0jLE3eA-DTXo6prCfNRr1BduvMN9ZjgUfT';
 ?>
